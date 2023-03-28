18c08285e853e76e1233d726d89f35526ee8eed6
ranggasuryap15 <ranggasurya.313@gmail.com>
2023-03-28 23:52:50 +0700

    teknik penamaan variabel pada golang

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	
+	var (
+		umur int
+		alamat string
+		isMarried bool
+	)
+
+	umur = 10
+	alamat = "tambun bekasi"
+	isMarried = false
+
+	fmt.Println("umur", umur, "alamat", alamat, "udah nikah?", isMarried)
+
+	// variabel harus dipakai, jika tidak maka bisa di ignore dengan tanda underscore
+	test1, _ := 10, 10
+	fmt.Println(test1)
+
+	// PascalCase atau camelCase variable name
+	MyFamily := 10
+	myAddress := "teks"
+	fmt.Println(myAddress)
+	fmt.Println(MyFamily)
+
 }
